6360e316b92213caba2205d6d175b315b1f1d9d1
Alexander Casall <alexander.casall@saxsys.de>
2014-08-08 08:37:42 +0200

    Added Error syso / Log if the application could not startup for the test

--- src/main/java/de/saxsys/javafx/test/SingleJfxApplication.java
+++ src/main/java/de/saxsys/javafx/test/SingleJfxApplication.java
@@ -8,11 +8,12 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.ReentrantLock;
-
-import org.junit.runners.model.InitializationError;
+import java.util.logging.Logger;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import org.junit.runners.model.InitializationError;
 
 /**
  * This is the application which starts JavaFx. It is controlled through the
@@ -20,62 +21,65 @@ import javafx.stage.Stage;
  */
 public class SingleJfxApplication extends Application {
 
-	/** The lock that guarantees that only one JavaFX thread will be started. */
-	private static final ReentrantLock LOCK = new ReentrantLock();
+    /** The lock that guarantees that only one JavaFX thread will be started. */
+    private static final ReentrantLock LOCK = new ReentrantLock();
 
-	/** Started flag. */
-	private static AtomicBoolean started = new AtomicBoolean();
+    /** Started flag. */
+    private static AtomicBoolean started = new AtomicBoolean();
 
-	/**
-	 * Start JavaFx.
-	 * @throws InitializationError 
-	 * @throws InterruptedException 
-	 */
-	public static void startJavaFx() throws InitializationError {
-		try {
-			// Lock or wait. This gives another call to this method time to
-			// finish
-			// and release the lock before another one has a go
-			LOCK.lock();
+    /**
+     * Start JavaFx.
+     * 
+     * @throws InitializationError
+     * @throws InterruptedException
+     */
+    public static void startJavaFx() throws InitializationError {
+        try {
+            // Lock or wait. This gives another call to this method time to
+            // finish
+            // and release the lock before another one has a go
+            LOCK.lock();
 
-			if (!started.get()) {
-				// start the JavaFX application
-				final ExecutorService executor = Executors.newSingleThreadExecutor();
-				Future<?> jfxLaunchFuture = executor.submit(() -> SingleJfxApplication.launch());
+            if (!started.get()) {
+                // start the JavaFX application
+                final ExecutorService executor = Executors.newSingleThreadExecutor();
+                Future<?> jfxLaunchFuture = executor.submit(() -> SingleJfxApplication.launch());
 
-				while (!started.get()) {
-					try {
-						jfxLaunchFuture.get(1, TimeUnit.MILLISECONDS);
-					} catch (InterruptedException | TimeoutException e) {
-						//continue waiting until success or error
-					}
-						
-					
-					Thread.yield();
-				}
-			}
-		} catch (ExecutionException e) {
-			throw new InitializationError(e);
-		} finally {
-			LOCK.unlock();
-		}
-	}
-	
-	/**
-	 * Launch.
-	 */
-	protected static void launch() {
-		Application.launch();
-	}
+                while (!started.get()) {
+                    try {
+                        // there is an Issue, if the Application needs more than
+                        // a second to start
+                        jfxLaunchFuture.get(1, TimeUnit.SECONDS);
+                    } catch (InterruptedException | TimeoutException e) {
+                        System.err.println("Could not launce JavaFX Application.");
+                        Logger.getGlobal().severe("Could not launce JavaFX Application.");
+                    }
 
-	/**
-	 * An empty start method.
-	 *
-	 * @param stage
-	 *            The stage
-	 */
-	@Override
-	public void start(final Stage stage) {
-		started.set(Boolean.TRUE);
-	}
+                    Thread.yield();
+                }
+            }
+        } catch (ExecutionException e) {
+            throw new InitializationError(e);
+        } finally {
+            LOCK.unlock();
+        }
+    }
+
+    /**
+     * Launch.
+     */
+    protected static void launch() {
+        Application.launch();
+    }
+
+    /**
+     * An empty start method.
+     *
+     * @param stage
+     *            The stage
+     */
+    @Override
+    public void start(final Stage stage) {
+        started.set(Boolean.TRUE);
+    }
 }
