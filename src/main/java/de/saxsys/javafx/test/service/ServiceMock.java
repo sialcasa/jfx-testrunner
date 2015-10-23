5c143279cbf5ae69538b8c17ea9fe98b5600b7ba
Alexander Casall <alexander.casall@gmail.com>
2015-10-24 01:23:02 +0200

    Removed Interface implementation of ServiceMock to be able to delete unescessary methods and hide the setters of the mock object

--- src/main/java/de/saxsys/javafx/test/service/ServiceMock.java
+++ src/main/java/de/saxsys/javafx/test/service/ServiceMock.java
@@ -3,7 +3,6 @@ package de.saxsys.javafx.test.service;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
@@ -11,8 +10,9 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.concurrent.Service;
 import javafx.concurrent.Worker;
+import javafx.concurrent.Worker.State;
 
-public class ServiceMock<V> implements Worker<V> {
+public class ServiceMock<V> {
 	
 	private final ObjectProperty<State> state = new SimpleObjectProperty<>(this, "state", State.READY);
 	private final ObjectProperty<V> value = new SimpleObjectProperty<>(this, "value");
@@ -37,142 +37,82 @@ public class ServiceMock<V> implements Worker<V> {
 	}
 	
 	
-	@Override
-	public final ObjectProperty<State> stateProperty() {
-		return this.state;
+	public final Worker.State getState() {
+		return state.get();
 	}
 	
-	@Override
-	public final javafx.concurrent.Worker.State getState() {
-		return this.stateProperty().get();
+	private final void setState(final javafx.concurrent.Worker.State state) {
+		this.state.set(state);
 	}
 	
-	public final void setState(final javafx.concurrent.Worker.State state) {
-		this.stateProperty().set(state);
-	}
-	
-	@Override
-	public final ObjectProperty<V> valueProperty() {
-		return this.value;
-	}
-	
-	@Override
 	public final V getValue() {
-		return this.valueProperty().get();
+		return value.get();
 	}
 	
-	public final void setValue(final V value) {
-		this.valueProperty().set(value);
+	private final void setValue(final V value) {
+		this.value.set(value);
 	}
 	
-	@Override
-	public final ObjectProperty<Throwable> exceptionProperty() {
-		return this.exception;
+	public final Throwable getException() {
+		return this.exception.get();
 	}
 	
-	@Override
-	public final java.lang.Throwable getException() {
-		return this.exceptionProperty().get();
+	private final void setException(final Throwable exception) {
+		this.exception.set(exception);
 	}
 	
-	public final void setException(final java.lang.Throwable exception) {
-		this.exceptionProperty().set(exception);
-	}
-	
-	@Override
-	public final DoubleProperty workDoneProperty() {
-		return this.workDone;
-	}
-	
-	@Override
 	public final double getWorkDone() {
-		return this.workDoneProperty().get();
+		return this.workDone.get();
 	}
 	
-	public final void setWorkDone(final double workDone) {
-		this.workDoneProperty().set(workDone);
+	private final void setWorkDone(final double workDone) {
+		this.workDone.set(workDone);
 	}
 	
-	public final DoubleProperty totalWorkToBeDoneProperty() {
-		return this.totalWorkToBeDone;
-	}
 	
 	public final double getTotalWorkToBeDone() {
-		return this.totalWorkToBeDoneProperty().get();
+		return this.totalWorkToBeDone.get();
 	}
 	
-	public final void setTotalWorkToBeDone(final double totalWorkToBeDone) {
-		this.totalWorkToBeDoneProperty().set(totalWorkToBeDone);
+	private final void setTotalWorkToBeDone(final double totalWorkToBeDone) {
+		this.totalWorkToBeDone.set(totalWorkToBeDone);
 	}
 	
-	@Override
-	public final DoubleProperty progressProperty() {
-		return this.progress;
-	}
-	
-	@Override
 	public final double getProgress() {
-		return this.progressProperty().get();
+		return this.progress.get();
 	}
 	
-	public final void setProgress(final double progress) {
-		this.progressProperty().set(progress);
+	private final void setProgress(final double progress) {
+		this.progress.set(progress);
 	}
 	
-	@Override
-	public final BooleanProperty runningProperty() {
-		return this.running;
-	}
 	
-	@Override
 	public final boolean isRunning() {
-		return this.runningProperty().get();
+		return this.running.get();
 	}
 	
-	public final void setRunning(final boolean running) {
-		this.runningProperty().set(running);
+	private final void setRunning(final boolean running) {
+		this.running.set(running);
 	}
 	
-	@Override
-	public final StringProperty messageProperty() {
-		return this.message;
+	public final String getMessage() {
+		return this.message.get();
 	}
 	
-	@Override
-	public final java.lang.String getMessage() {
-		return this.messageProperty().get();
+	private final void setMessage(final String message) {
+		this.message.set(message);
 	}
 	
-	public final void setMessage(final java.lang.String message) {
-		this.messageProperty().set(message);
+	
+	public final String getTitle() {
+		return this.title.get();
 	}
 	
-	@Override
-	public final StringProperty titleProperty() {
-		return this.title;
+	private final void setTitle(final String title) {
+		this.title.set(title);
 	}
 	
-	@Override
-	public final java.lang.String getTitle() {
-		return this.titleProperty().get();
-	}
-	
-	public final void setTitle(final java.lang.String title) {
-		this.titleProperty().set(title);
-	}
-	
-	@Override
 	public double getTotalWork() {
 		return totalWorkToBeDone.get();
-	}
-	
-	@Override
-	public ReadOnlyDoubleProperty totalWorkProperty() {
-		return totalWorkToBeDone;
-	}
-	
-	@Override
-	public boolean cancel() {
-		throw new RuntimeException("This method can't be used.");
 	}
 }
